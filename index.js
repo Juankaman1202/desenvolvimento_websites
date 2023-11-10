ec50e6f4b5a4e2ac04429d90e956161f4dd3f7ec
Juan Kaman <juan.kaman@eaportal.org>
2023-11-10 09:51:13 -0300

    pronto

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { application } = require("express")
 const express = require("express")
+
+const path = require("path")
+
+const app = express()
+
+const CaminhoBase = path.join(__dirname, "templates") 
+
 application.get('/' , (requisicao,respota) =>{ 
-    respota.send("ESTOU FUNCIONANDO COM O EXPRESS")
+    respota.sendFile(`${CaminhoBase}/index.html`)
 })
 
 application.listen(3000, () =>{
    console.log("servidor rodando na porta 3000!") 
-})
+})  
